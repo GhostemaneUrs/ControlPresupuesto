e1944653536280ed33a61aa8e00bf42d42a3513d
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-22 14:43:33 -0500

    feat: update inglish component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 const Header = () => {
   return (
     <div className={styles.container_header}>
-      <h1 className={styles.title}>Planificador De Gastos</h1>
+      <h1 className={styles.title}>Expense Planner</h1>
     </div>
   );
 };
