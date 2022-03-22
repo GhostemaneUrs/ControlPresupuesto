59e6bf2f3ff906873810939b758ba402c8af84e7
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-22 14:44:56 -0500

    feat: new budget logic

--- src/components/NewBudget/NewBudget.jsx
+++ src/components/NewBudget/NewBudget.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Message from "../Message/Message";
 
-const NewBudget = () => {
+const NewBudget = ({ budget, setBudget, setIsValidBudget }) => {
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setBudget(Number(e.target.value));
+  };
+
+  const onSubmitBudget = (e) => {
+    e.preventDefault();
+    if (!budget || budget < 0) {
+      setMessage("Please enter a valid budget");
+      setBudget(0);
+      return;
+    }
+    setMessage("");
+    setIsValidBudget(true);
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-      <form action="" className="formulario">
+      <form action="" className="formulario" onSubmit={onSubmitBudget}>
         <div className="campo">
-          <label htmlFor="">Definir Presupuesto</label>
+          <label htmlFor="">Define Budget</label>
           <input
+            type="number"
+            value={budget}
             className="nuevo-presupuesto"
-            type="text"
-            placeholder="Añade tu prespuesto"
+            placeholder="Enter Your Budget"
+            onChange={handleChange}
           />
         </div>
-        <input type="submit" value="Añadir" />
+        <input type="submit" value="Add your budget" />
+        {message && <Message type="error"> {message} </Message>}
       </form>
     </div>
   );
