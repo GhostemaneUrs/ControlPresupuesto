8b450d72901a6e3aaa9afcc52a86b5af60d35847
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-25 13:29:37 -0500

    feat: implement useEffect to manage cost control

--- src/components/BudgetControl/BudgetControl.jsx
+++ src/components/BudgetControl/BudgetControl.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { formatAmount } from "../../helpers/functions";
 
-const BudgetControl = ({ budget }) => {
+const BudgetControl = ({ budget, expenses }) => {
+  const [availableBudget, setAvailableBudget] = useState(0);
+  const [spent, setSpent] = useState(0);
+  useEffect(() => {
+    const totalSpent = expenses.reduce(
+      (total, spent) => spent.amount + total,
+      0
+    );
+    const totalAvailable = budget - totalSpent;
+    setSpent(totalSpent);
+    setAvailableBudget(totalAvailable);
+  }, [expenses]);
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -8,7 +20,13 @@ const BudgetControl = ({ budget }) => {
       </div>
       <div className="contenido-presupuesto">
         <p>
-          <span>Budget: </span> ${budget}
+          <span>Budget: </span> {formatAmount(budget)}
+        </p>
+        <p>
+          <span>Available: </span> {formatAmount(availableBudget)}
+        </p>
+        <p>
+          <span>Spent: </span> {formatAmount(spent)}
         </p>
       </div>
     </div>
