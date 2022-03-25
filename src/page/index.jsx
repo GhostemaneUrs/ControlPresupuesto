226fbab976a9b0a3e0d9f1abe10ee0e69b020395
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-25 13:31:00 -0500

    feat: implement logic components expense control

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -2,25 +2,66 @@ import React, { useState } from "react";
 import Header from "../components/Header/Header";
 import NewBudget from "../components/NewBudget/NewBudget";
 import BudgetControl from "../components/BudgetControl/BudgetControl";
+import ListBudget from "../components/ListBudget/ListBudget";
+import NewBudgetModal from "../components/NewBudgetModal/NewBudgetModal";
+import newBudgetImg from "../assets/img/nuevo-gasto.svg";
 
 const index = () => {
   const [budget, setBudget] = useState(0);
+  const [expenses, setExpenses] = useState([]);
   const [isValidBudget, setIsValidBudget] = useState(false);
+  const [isActiveModal, setIsActiveModal] = useState(false);
+  const [isAnimateModal, setIsAnimateModal] = useState(false);
+
+  const handleNewBudget = () => {
+    setIsActiveModal(true);
+    setTimeout(() => {
+      setIsAnimateModal(true);
+    }, 500);
+  };
+
+  const saveExpense = (expense) => {
+    setExpenses([...expenses, expense]);
+    setIsAnimateModal(false);
+    setTimeout(() => {
+      setIsActiveModal(false);
+    }, 500);
+  };
   return (
     <>
-      <div className="container-bg">
-        <Header />
-        <div className="container-1400">
-          {isValidBudget ? (
-            <BudgetControl budget={budget} />
-          ) : (
-            <NewBudget
-              budget={budget}
-              setBudget={setBudget}
-              setIsValidBudget={setIsValidBudget}
-            />
-          )}
+      <div className={isActiveModal ? "fijar" : ""}>
+        <div className="container-bg">
+          <Header />
+          <div className="container-1400">
+            {isValidBudget ? (
+              <>
+                <BudgetControl budget={budget} expenses={expenses}/>
+                <div className="nuevo-gasto">
+                  <img
+                    src={newBudgetImg}
+                    alt="Nuevo gasto"
+                    onClick={handleNewBudget}
+                  />
+                </div>
+                {isActiveModal && (
+                  <NewBudgetModal
+                    isAnimateModal={isAnimateModal}
+                    saveExpense={saveExpense}
+                    setIsAnimateModal={setIsAnimateModal}
+                    setIsActiveModal={setIsActiveModal}
+                  />
+                )}
+              </>
+            ) : (
+              <NewBudget
+                budget={budget}
+                setBudget={setBudget}
+                setIsValidBudget={setIsValidBudget}
+              />
+            )}
+          </div>
         </div>
+        <div>{isValidBudget ? <ListBudget expenses={expenses} /> : null}</div>
       </div>
     </>
   );
