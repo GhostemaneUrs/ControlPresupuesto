49416e343a6965a20625c86285836b041afb8027
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-04-05 11:32:58 -0500

    feat: update structure list budget and implement list filter

--- src/components/ListBudget/ListBudget.jsx
+++ src/components/ListBudget/ListBudget.jsx
@@ -16,7 +16,13 @@ import healthIcon from "../../assets/img/icono_salud.svg";
 import subscriptionsIcon from "../../assets/img/icono_suscripciones.svg";
 import miscellaniousExpenseIcon from "../../assets/img/icono_gastos.svg";
 
-const ListBudget = ({ expenses, setExpensesEdit, deleteExpense }) => {
+const ListBudget = ({
+  expenses,
+  expensesFilter,
+  filter,
+  setExpensesEdit,
+  deleteExpense,
+}) => {
   const dictionaryIcon = {
     saving: savingIcon,
     home: homeIcon,
@@ -41,38 +47,70 @@ const ListBudget = ({ expenses, setExpensesEdit, deleteExpense }) => {
     </TrailingActions>
   );
   return (
-    <main>
-      <div className="listado-gastos contenedor">
-        <h2>{expenses.length ? "Expenses" : "You have no expenses yet"}</h2>
-        <div className="scroll">
-          {expenses.map((expense) => (
-            <SwipeableList key={expense.id}>
-              <SwipeableListItem
-                leadingActions={leadingActions(expense)}
-                trailingActions={trailingActions(expense)}
-              >
-                <div className="gasto sombra">
-                  <div className="contenido-gasto">
-                    <img src={dictionaryIcon[expense.category]} alt="icono" />
-                    <div className="descripcion-gasto">
-                      <p className="categoria">{expense.category}</p>
-                      <p className="nombre-gasto"> {expense.expenseName}</p>
-                      <p className="fecha-gasto">
-                        {" "}
-                        Agregado el: {""} {expense.date}
-                      </p>
+    <div className="listado-gastos contenedor">
+      <h2>
+        {filter
+          ? expensesFilter.length
+            ? "Expenses"
+            : "There are no expenses in this category"
+          : expenses.length
+          ? "Expenses"
+          : "You don't have any expenses"}
+      </h2>
+      <div className="scroll">
+        {filter
+          ? expensesFilter.map((expense) => (
+              <SwipeableList key={expense.id}>
+                <SwipeableListItem
+                  leadingActions={leadingActions(expense)}
+                  trailingActions={trailingActions(expense)}
+                >
+                  <div className="gasto sombra-card">
+                    <div className="contenido-gasto">
+                      <img src={dictionaryIcon[expense.category]} alt="icono" />
+                      <div className="descripcion-gasto">
+                        <p className="categoria">{expense.category}</p>
+                        <p className="nombre-gasto"> {expense.expenseName}</p>
+                        <p className="fecha-gasto">
+                          {" "}
+                          Agregado el: {""} {expense.date}
+                        </p>
+                      </div>
                     </div>
+                    <p className="cantidad-gasto">
+                      {formatAmount(expense.amount)}
+                    </p>
                   </div>
-                  <p className="cantidad-gasto">
-                    {formatAmount(expense.amount)}
-                  </p>
-                </div>
-              </SwipeableListItem>
-            </SwipeableList>
-          ))}
-        </div>
+                </SwipeableListItem>
+              </SwipeableList>
+            ))
+          : expenses.map((expense) => (
+              <SwipeableList key={expense.id}>
+                <SwipeableListItem
+                  leadingActions={leadingActions(expense)}
+                  trailingActions={trailingActions(expense)}
+                >
+                  <div className="gasto sombra-card">
+                    <div className="contenido-gasto">
+                      <img src={dictionaryIcon[expense.category]} alt="icono" />
+                      <div className="descripcion-gasto">
+                        <p className="categoria">{expense.category}</p>
+                        <p className="nombre-gasto"> {expense.expenseName}</p>
+                        <p className="fecha-gasto">
+                          {" "}
+                          Agregado el: {""} {expense.date}
+                        </p>
+                      </div>
+                    </div>
+                    <p className="cantidad-gasto">
+                      {formatAmount(expense.amount)}
+                    </p>
+                  </div>
+                </SwipeableListItem>
+              </SwipeableList>
+            ))}
       </div>
-    </main>
+    </div>
   );
 };
 
